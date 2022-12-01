34a893dac5a33e55093e834c8cd65f37d9b4fe25
Guido Kaloustian <guidojk96@gmail.com>
2022-12-01 16:08:30 -0300

    Agregado de items al carrito y badge del icono cart

--- React/e-commerce/src/App.js
+++ React/e-commerce/src/App.js
@@ -2,17 +2,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ItemListContainer from "./containers/ItemListContainer";
 import ItemDetailContainer from "./containers/ItemDetailContainer";
 import Navbar from "./components/NavBar";
+import Cart from "./components/Cart";
+import CartContextProvider from "./components/CartContext";
 
 const App = (props) => {
   return (
-    <BrowserRouter>
-      <Navbar />
-        <Routes>
-          <Route path='/' element={<ItemListContainer greeting='¡Bienvenidos a H&H!' />}/>
-          <Route path='/category/:categoryID' element={<ItemListContainer/>}/>
-          <Route path='/item/:itemID' element={<ItemDetailContainer/>}/>
-        </Routes>
-    </BrowserRouter>
+    <CartContextProvider>
+      <BrowserRouter>
+        <Navbar />
+          <Routes>
+            <Route path='/' element={<ItemListContainer greeting='¡Bienvenidos a H&H!' />}/>
+            <Route path='/category/:categoryID' element={<ItemListContainer/>}/>
+            <Route path='/item/:itemID' element={<ItemDetailContainer/>}/>
+            <Route path='/cart' element={<Cart/>}/>
+          </Routes>
+      </BrowserRouter>
+    </CartContextProvider>
   );
 }
 
--- React/e-commerce/src/components/CartWidget.jsx
+++ React/e-commerce/src/components/CartWidget.jsx
@@ -1,11 +1,21 @@
 import carrito from '../img/carrito.png';
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { CartContext } from './CartContext';
 
 const CartWidget = () => {
+  const context = useContext(CartContext);
+
     return (
       <>
         <div className="carrito-img">
+          <Link className='link' to='/cart'>
           <img id="carrito" src={carrito} alt='...'/>
-          <p className="badge">1</p>
+          {
+            context.cartList.length > 0 &&
+            <p className="badge">{context.totalQty()}</p>
+          }
+          </Link>
         </div>   
       </>
     );
